e287a49dcdeff5c403b477fe1fd5caa3e4fdcfce
mussab ahmed <mussab87@gmail.com>
2022-03-26 00:38:54 +0300

    last update

--- GreenFactory/Startup.cs
+++ GreenFactory/Startup.cs
@@ -76,6 +76,7 @@ namespace GreenFactory
         {
             if (env.IsDevelopment())
             {
+                //app.UseExceptionHandler("/Home/Error");
                 app.UseDeveloperExceptionPage();
             }
             else
